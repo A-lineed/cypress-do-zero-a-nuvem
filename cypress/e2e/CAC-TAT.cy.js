dca35ce96a8f49a3f17d9f1c1518d21e42308706
A-lineed <aline.edvania.franca@outlook.com>
2025-03-25 17:08:31 -0300

    Realizando preenchimento de campos e clique em botões

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -1,10 +1,29 @@
 describe('Central de atendimento ao cliente TAT', () => {
-  it('Verifica o título da aplicação', () => {
+
+  beforeEach(() => {
     cy.visit('src/index.html')
+  })
+
+
+  it('Verifica o título da aplicação', () => {
+
     //Verificando se título do site é igual
     cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
 
     //Verificando que o título não é igual
     cy.title().should('not.be.equal', 'Central de Atendimento')
+  })
+
+
+  it('Preenche os campos obrigatórios e envia o formulário', () => {
+    cy.get('#firstName').type('Aline')
+    cy.get('#lastName').type('Edvania')
+    cy.get('#email').type('aline.franca@gmail.com')
+    cy.get('#open-text-area').type('Preciso de ajuda com os testes')
+    cy.get('button[type="submit"]').click()
+    
+
+    cy.get('.success').should('be.visible');
+
   })
 })
