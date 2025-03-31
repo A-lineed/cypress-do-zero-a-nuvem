679b305b370b1feb03833462ac21fa3722296651
A-lineed <aline.edvania.franca@outlook.com>
2025-03-31 09:00:58 -0300

    Selecionando arquivo utilizando uma fixture a qual foi dada um alias

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -183,11 +183,21 @@ describe('Central de atendimento ao cliente TAT', () => {
       })
   })
 
-  it.only('Seleciona um arquivo simulando um drag-and-drop', () => {
+  it('Seleciona um arquivo simulando um drag-and-drop', () => {
     cy.get('#file-upload')
       .selectFile('cypress/fixtures/example.json', { action: 'drag-drop' })
       .should((input) => {
         //console.log(input[0].files[0].name) -> Verifica se o nome do arquivo está sendo retornado corretamente
+        expect(input[0].files[0].name).to.equal('example.json')
+
+      })
+  })
+
+  it.only('Seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', () => {
+    cy.fixture('example.json').as('sampleFile')
+    cy.get('#file-upload')
+      .selectFile('@sampleFile')
+      .should((input) => {
         expect(input[0].files[0].name).to.equal('example.json')
 
       })
