b8936509f6347e2cc11c063e14e5b695b056853d
A-lineed <aline.edvania.franca@outlook.com>
2025-04-03 16:42:36 -0300

    Configuração do cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,10 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   //viewportHeight: 880,
  // viewportWidth: 1280,
+  projectId: "y618p5",
   e2e: {},
   video:false,
+ 
 
 })
 
